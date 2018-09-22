ff6f735c697bd08f2c00dbd98f8cb1c5f38361b3
Ryan Valentin <ryanvalentin@outlook.com>
2018-09-22 12:18:55 -0700

    switch /posts with /news

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,8 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                path: `${__dirname}/src/posts`,
-                name: 'posts',
+                path: `${__dirname}/src/news`,
+                name: 'news',
             },
         },
 
