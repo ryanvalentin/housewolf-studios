4a0686981e1add343726b2b5a50e06a4a1d6fa76
Ryan Valentin <ryanvalentin@outlook.com>
2018-09-22 22:35:22 -0700

    use Layout for Post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Layout from '../components/layout';
 
-export default function Template({ data /* this prop will be injected by the GraphQL query below. */ }) {
+export default function Template({ data }) {
     const { markdownRemark } = data; // data.markdownRemark holds our post data
     const { frontmatter, html } = markdownRemark;
 
     return (
-        <div>
+        <Layout>
             <div>
                 <h1>{frontmatter.title}</h1>
                 <h2>{frontmatter.date}</h2>
                 <div dangerouslySetInnerHTML={{ __html: html }} />
             </div>
-        </div>
+        </Layout>
     );
 }
 
